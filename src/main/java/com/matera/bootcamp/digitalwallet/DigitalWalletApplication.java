9a4527c83a78e3d3ff41098c82022058e2ce90ac
CarolineCamargo <caroline.flach@hotmail.com.br>
2023-03-02 20:51:09 -0300

    First commit

---
+++ src/main/java/com/matera/bootcamp/digitalwallet/DigitalWalletApplication.java
@@ -0,0 +1,14 @@
+package com.matera.bootcamp.digitalwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalWalletApplication.class, args);
+	}
+
+}
+
